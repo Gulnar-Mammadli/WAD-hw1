7a98300b05a9e4677ead74d5d0a54ee6b26e701b
mammadli <gulnar.mammadli@ut.ee>
2023-10-03 23:49:14 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,13 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-submit");
 
-loginButton.addEventListener("click", (e) => {
+loginForm.addEventListener("sumbit", (e) => {
     e.preventDefault();
     const email = loginForm.email.value;
     const password = loginForm.password.value;
 
     if (email && password) {
         alert("You have successfully logged in.");
-        // location.reload();
         location.href = 'index.html';
     } else {
         alert("You could not log in! Missing credentials.");
