66d4761254b52bc6aac6ed4a5860c51dbb6ab2c4
LeylaRahimli <leyla.xalilovaa@gmail.com>
2023-11-05 23:21:11 +0200

    update dropdown

--- res/js/dropdown.js
+++ res/js/dropdown.js
@@ -1,16 +1,5 @@
-function myFunction() {
-  document.getElementById("dropdown").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-  if (!event.target.closest(".dropbtn")) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
+let btn = document.querySelector(".dropbtn");
+let dropdown = btn.nextElementSibling;
+btn.addEventListener("click", function () {
+    dropdown.classList.toggle("show");
+});
